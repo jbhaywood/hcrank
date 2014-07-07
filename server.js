bca8cda2fa4494aaa2ca297557258361b83b6a26
James Haywood <james@jhaywood.com>
2014-07-07 09:21:11 -0400

    debugging heroku error R10 by trying to speed up start time

--- server.js
+++ server.js
@@ -8,66 +8,73 @@ var mongoose = require('mongoose');
 var db_server = 'test_db';
 mongoose.connection.on("connected", function(ref) {
     console.log("Connected to " + db_server + " DB!");
-    
-    cardProvider.initialize();
-    dbProvider.initialize();
-    
-    var app = express();
-    var server = http.createServer(app);
-    
-    app.use(express.static(path.resolve(__dirname, 'public')));
-    app.use(express.json());
-    app.use(express.urlencoded());
-    
-    app.get('/api/newmatchup/', function(req, res) {
-        var data = cardProvider.getTwoRandomNeutralCards();
-        res.send(data);
-    });
-    
-    app.post('/api/sendmatchup/', function(req, res) {
-        var matchupData = req.body;
-        var promise1 = dbProvider.getRank(matchupData.cardOneId);
-        promise1.onFulfill(function(rankOne) {
-            var promise2 = dbProvider.getRank(matchupData.cardTwoId);
-            promise2.onFulfill(function(rankTwo) {
-                var pickedCardId;
-                var pickedNewRank;
-                var unpickedCardId;
-                var unpickedNewRank;
-                if (matchupData.picked == 1) {
-                    pickedCardId = matchupData.cardOneId;
-                    pickedNewRank = ((100 - rankOne) / 2) + rankOne;
-                    unpickedCardId = matchupData.cardTwoId;
-                    unpickedNewRank = rankTwo - (rankTwo / 2);
-                } else {
-                    pickedCardId = matchupData.cardTwoId;
-                    pickedNewRank = ((100 - rankTwo) / 2) + rankTwo;
-                    unpickedCardId = matchupData.cardOneId;
-                    unpickedNewRank = rankOne - (rankOne / 2);
-                }
-                dbProvider.setNeutralRank(pickedCardId, pickedNewRank);
-                dbProvider.setNeutralRank(unpickedCardId, unpickedNewRank);
-                dbProvider.saveMatchup(matchupData.cardOneId, matchupData.cardTwoId, pickedCardId, matchupData.time);
-                
-                var best = rankOne >= rankTwo ? 1 : 2;
-                var pickedBest = matchupData.picked == best;
-                
-                res.send({ 
-                    pickedBest: pickedBest
-                });
+});
+
+try {
+    mongoose.connect('mongodb://jhaywood:test_db@kahana.mongohq.com:10037/test_db');
+    console.log("Trying to connect to DB " + db_server);
+} catch (err) {
+    console.log("Sever initialization failed " , err.message);
+}
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/api/newmatchup/', function(req, res) {
+    var data = cardProvider.getTwoRandomNeutralCards();
+    res.send(data);
+});
+
+app.post('/api/sendmatchup/', function(req, res) {
+    var matchupData = req.body;
+    var promise1 = dbProvider.getRank(matchupData.cardOneId);
+    promise1.onFulfill(function(rankOne) {
+        var promise2 = dbProvider.getRank(matchupData.cardTwoId);
+        promise2.onFulfill(function(rankTwo) {
+            var pickedCardId;
+            var pickedNewRank;
+            var unpickedCardId;
+            var unpickedNewRank;
+            if (matchupData.picked == 1) {
+                pickedCardId = matchupData.cardOneId;
+                pickedNewRank = ((100 - rankOne) / 2) + rankOne;
+                unpickedCardId = matchupData.cardTwoId;
+                unpickedNewRank = rankTwo - (rankTwo / 2);
+            } else {
+                pickedCardId = matchupData.cardTwoId;
+                pickedNewRank = ((100 - rankTwo) / 2) + rankTwo;
+                unpickedCardId = matchupData.cardOneId;
+                unpickedNewRank = rankOne - (rankOne / 2);
+            }
+            dbProvider.setNeutralRank(pickedCardId, pickedNewRank);
+            dbProvider.setNeutralRank(unpickedCardId, unpickedNewRank);
+            dbProvider.saveMatchup(matchupData.cardOneId, matchupData.cardTwoId, pickedCardId, matchupData.time);
+            
+            var best = rankOne >= rankTwo ? 1 : 2;
+            var pickedBest = matchupData.picked == best;
+            
+            res.send({ 
+                pickedBest: pickedBest
             });
         });
     });
-    
-    var server_port = process.env.PORT || 8080;
-    var server_ip_address = process.env.IP || '127.0.0.1';
-
-    server.listen(server_port, server_ip_address, function(){
-        var addr = server.address();
-        console.log("Server listening at", addr.address + ":" + addr.port);
-    });
 });
  
+cardProvider.initialize();
+dbProvider.initialize();
+
+var server_port = process.env.PORT || 8080;
+var server_ip_address = process.env.IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function(){
+    var addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+});
+    
 // If the connection throws an error
 mongoose.connection.on("error", function(err) {
   console.error('Failed to connect to DB ' + db_server + ' on startup ', err);
@@ -87,10 +94,3 @@ var gracefulExit = function() {
  
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
-
-try {
-    mongoose.connect('mongodb://jhaywood:test_db@kahana.mongohq.com:10037/test_db');
-    console.log("Trying to connect to DB " + db_server);
-} catch (err) {
-    console.log("Sever initialization failed " , err.message);
-}
